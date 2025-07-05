4b6fdecbfc57f39d9c40222dd74afc456cb8b345
Gabriel Francisco Ventura Moreno <104277821+gabsss1@users.noreply.github.com>
2025-07-05 19:55:44 +0000

    test

--- data/ejemplo_vulnerable.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <string.h>
-
-void vulnerable() {
-    char buffer[10];
-    gets(buffer); // ⚠️ Esta función es insegura (buffer overflow)
-    printf("Input: %s\n", buffer);
-}
